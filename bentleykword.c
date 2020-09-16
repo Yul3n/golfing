d466c0393d21d10e1e13c63f04493ff0ef954328
Yul3n <yul3n.falx@protonmail.com>
2020-09-16 17:50:26 +0200

    Continued.

--- bentleykword.c
+++ bentleykword.c
@@ -7,21 +7,32 @@ typedef struct node {
 	int i;
 } Node;
 
-static void node_push(Node);
+static char *word_for(int);
 static int create_child(int, char);
 static int find_child(int, char);
 
 Node *node;
 long  node_len = 0, max_node_len;
 
-static void
-node_push(Node n)
+static char *
+word_for(int p)
 {
-	if (++node_len >= max_node_len) {
-		max_node_len += 600;
-		node          = realloc(node, (size_t)max_node_len);
+	int   size, i, c;
+	char *s;
+
+	i    = 0;
+	size = 256;
+	s    = malloc(size);
+	while (p != 0) {
+		c = p % 27;
+		if (++i >= size) {
+			size += 256;
+			s     = realloc(s, size);
+		}
+		*(s + i) = c;
+		p = (p - c) ? node[p - c].p : 0;
 	}
-	*(node + node_len - 1) = n;
+	return s;
 }
 
 static int
@@ -29,11 +40,11 @@ create_child(int p, char c)
 {
 	int len;
 
-	len = max_node_len;
+	len           = max_node_len;
 	max_node_len += 27;
-	node = realloc((void *)node, max_node_len);
-	node[len] = (Node){p, -1};
-	node[p].p = len;
+	node          = realloc((void *)node, max_node_len);
+	node[len]     = (Node){p, -1};
+	node[p].p     = len;
 	return len + c;
 }
 
@@ -41,7 +52,7 @@ static int
 find_child(int p, char c)
 {
 	if (p == 0)
-		return c;	
+		return c;
 	else if (node[p].p == 0)
 		return create_child(p, c);
 	else
@@ -54,7 +65,7 @@ main(int argc, char **argv)
 	FILE *in;
 	long  len;
 	char *data;
-	int p;
+	int   p;
 
 	/* Read the input file. Get the size of the file. */
 	in = fopen(argv[1], "rb");
@@ -66,15 +77,24 @@ main(int argc, char **argv)
 	fread(data, 1, len, in);
 	(void)fclose(in);
 
-	max_node_len = len / 600;
+	max_node_len = 27;
 	node         = (Node *)malloc(sizeof(Node) * max_node_len);
-	node_push((Node){0, 0});
-	for (char i = 0; i <= 26; ++i) node_push((Node){0, 0});
+	for (char i = 0; i <= 26; ++i)
+		*(node + i) = ((Node){0, 0});
+
 	p = 0;
 	for (int i = 0; i < len; ++i) {
 		char c = (data[i] | 32) - 'a' + 1; /* Get the alphabet index. */
-		if (1 <= c && 1 <= 26) {
+		if (1 <= c && 1 <= 26)
 			p = find_child(p, c);
+		else {
+			++node[p].i;
+			p = 0;
 		}
 	}
+	++node[p].i;
+	node[0].i = 0;
+
+	for (int i = 1; i < max_node_len; ++i)
+		int count = node[i].i;
 }
