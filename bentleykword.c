b9420343a74a2779293c977f83931027411ec5a5
Yul3n <yul3n.falx@protonmail.com>
2020-09-16 17:09:36 +0200

    Continued.

--- bentleykword.c
+++ bentleykword.c
@@ -8,16 +8,53 @@ typedef struct node {
 } Node;
 
 static void node_push(Node);
+static int create_child(int, char);
+static int find_child(int, char);
 
 Node *node;
-long node_len = 0, max_node_len;
+long  node_len = 0, max_node_len;
+
+static void
+node_push(Node n)
+{
+	if (++node_len >= max_node_len) {
+		max_node_len += 600;
+		node          = realloc(node, (size_t)max_node_len);
+	}
+	*(node + node_len - 1) = n;
+}
+
+static int
+create_child(int p, char c)
+{
+	int len;
+
+	len = max_node_len;
+	max_node_len += 27;
+	node = realloc((void *)node, max_node_len);
+	node[len] = (Node){p, -1};
+	node[p].p = len;
+	return len + c;
+}
+
+static int
+find_child(int p, char c)
+{
+	if (p == 0)
+		return c;	
+	else if (node[p].p == 0)
+		return create_child(p, c);
+	else
+		return node[p].p + c;
+}
 
 int
 main(int argc, char **argv)
 {
 	FILE *in;
-	long len;
+	long  len;
 	char *data;
+	int p;
 
 	/* Read the input file. Get the size of the file. */
 	in = fopen(argv[1], "rb");
@@ -29,6 +66,15 @@ main(int argc, char **argv)
 	fread(data, 1, len, in);
 	(void)fclose(in);
 
-	max_node_len = len / 600;	
-	node = (Node *)malloc(sizeof(Node) * max_node_len);
+	max_node_len = len / 600;
+	node         = (Node *)malloc(sizeof(Node) * max_node_len);
+	node_push((Node){0, 0});
+	for (char i = 0; i <= 26; ++i) node_push((Node){0, 0});
+	p = 0;
+	for (int i = 0; i < len; ++i) {
+		char c = (data[i] | 32) - 'a' + 1; /* Get the alphabet index. */
+		if (1 <= c && 1 <= 26) {
+			p = find_child(p, c);
+		}
+	}
 }
